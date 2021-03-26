f766eb93087252b21f2ec940cf1d156a010f0cfe
Clayton Sims <csims@dimagi.com>
2021-03-25 22:39:55 -0400

    sd bioline pf pv folios

--- app/src/main/java/org/rdtoolkit/model/diagnostics/StaticPamphlets.kt
+++ app/src/main/java/org/rdtoolkit/model/diagnostics/StaticPamphlets.kt
@@ -72,6 +72,22 @@ class StaticPamphlets(val context : Context) : PamphletSource {
             response.add(folio.getPamphlet())
         }
 
+        if (category.equals("reference") && tags.contains("sd_bioline_mal_pf_pv")) {
+            var folioContext = AssetFolioContext("bootstrapped/sd_bioline_mal_pf_pv/reference", context.assets)
+            var mediaContext = ZipStreamFolioContext(folioContext, "media.zip")
+            val folio = parseFolio(folioContext.spool("folio.json"), mediaContext)
+            folio.setLocale(locale.language)
+            response.add(folio.getPamphlet())
+        }
+
+        if (category.equals("interpret") && tags.contains("sd_bioline_mal_pf_pv")) {
+            var folioContext = AssetFolioContext("bootstrapped/sd_bioline_mal_pf_pv/interpret", context.assets)
+            var mediaContext = ZipStreamFolioContext(folioContext, "media.zip")
+            val folio = parseFolio(folioContext.spool("folio.json"), mediaContext)
+            folio.setLocale(locale.language)
+            response.add(folio.getPamphlet())
+        }
+
         return response
     }
 }
